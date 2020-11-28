ab293163784b8433149103688210cdea8cba788f
Beth <beth3346@gmail.com>
2020-11-28 12:38:14 -0600

    update button demo add gradient, glass and raised styles

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # elr-scss-buttons
 
-[![Build Status](https://travis-ci.com/Beth3346/elr-scss-buttons.svg?branch=master)](https://travis-ci.com/Beth3346/elr-scss-buttons)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 [![npm](https://img.shields.io/npm/dm/elr-scss-buttons.svg?style=flat)]()
 
@@ -11,7 +10,7 @@ some scss mixins for buttons
 Download node at [nodejs.org](http://nodejs.org) and install it, if you haven't already.
 
 ```sh
-npm install elr-scss-buttons --save
+npm install elr-scss-buttons -D
 yarn add elr-scss-buttons
 ```
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const path = require('path');
 const sass = require('gulp-sass');
+// const scsslint = require('gulp-scss-lint');
 const plumber = require('gulp-plumber');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
@@ -34,11 +35,18 @@ function clean() {
   return del(['./dist/css/']);
 }
 
+// Copy HTML
+function copyIndex() {
+  return gulp.src('./src/index.html')
+    .pipe(gulp.dest(paths.app))
+}
+
 // Watch files
 function watchFiles(done) {
-  gulp.watch(['./**/*.scss', 'dist/index.html'], () => {
+  gulp.watch(['./**/*.scss', './src/index.html'], () => {
     return gulp
       .src('./*.scss')
+      // .pipe(scsslint())
       .pipe(plumber())
       .pipe(
         sass({
@@ -48,13 +56,13 @@ function watchFiles(done) {
       .pipe(gulp.dest(paths.css))
       .pipe(
         autoprefixer({
-          browsers: ['last 3 versions'],
           cascade: false
         })
       )
       .pipe(gulp.dest(paths.css))
       .pipe(cleanCSS({ debug: true }))
       .pipe(gulp.dest(paths.css))
+      .pipe(copyIndex())
       .pipe(browserSyncReload(done));
   });
 }
@@ -71,16 +79,17 @@ gulp.task('default', () => {
     .pipe(gulp.dest(paths.css))
     .pipe(
       autoprefixer({
-        browsers: ['last 3 versions'],
         cascade: false
       })
     )
     .pipe(gulp.dest(paths.css))
     .pipe(cleanCSS({ debug: true }))
-    .pipe(gulp.dest(paths.css));
+    .pipe(gulp.dest(paths.css))
+    .pipe(copyIndex());
 });
 
-const watch = gulp.parallel(watchFiles, browserSync);
+const watch = gulp.parallel(copyIndex, watchFiles, browserSync);
 
 exports.clean = clean;
 exports.watch = watch;
+exports.copyIndex = copyIndex;
